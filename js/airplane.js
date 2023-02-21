3918ca045e57893319fcd0dd812879f107f9397e
Naod Alemu <naodalemu11@gmail.com>
2023-02-21 21:22:37 +0300

    Added some new things like a real text on the plane

--- js/airplane.js
+++ js/airplane.js
@@ -4,10 +4,52 @@ let airplaneContainer = $(".airplane-container");
 lax.init();
 lax.addDriver("scrollY", () => window.scrollY);
 lax.addElements(".airplane-container", {
-	scrollY: {
-		translateX: [
-			["elInY", "elCenterY", "elOutY"],
-			[200, "(-1 * screenWidth/2) + 300", "-1 * screenWidth + 300"],
-		],
-	},
+  scrollY: {
+    translateX: [
+      ["elInY", "elCenterY", "elOutY"],
+      [500, "(-1 * screenWidth/2) + 300", "-1 * screenWidth + 300"],
+    ],
+  },
 });
+
+// const $ = (selector) => document.querySelector(selector);
+
+// let airplaneContainer = $(".airplane-container");
+// let bgContainer = $(".bg-container");
+// lax.init();
+// lax.addDriver("scrollY", () => window.scrollY);
+// lax.addElements(".airplane-container", {
+//   scrollY: {
+//     translateX: [
+//       ["elInY", "elCenterY", "elOutY"],
+//       [200, "(-1 * screenWidth/2) + 300", "-1 * screenWidth + 300"],
+//     ],
+//   },
+// });
+// lax.addElements(".bg-container", {
+//   scrollY: {
+//     position: [
+//       [0, "elCenterY"],
+//       ["static", "fixed"],
+//     ],
+//   },
+// });
+
+// // Stop airplane animation after it reaches the end of the screen
+// let airplaneAnimationDone = false;
+// airplaneContainer.addEventListener("animationend", () => {
+//   airplaneAnimationDone = true;
+// });
+
+// // Scroll the background until the airplane animation starts
+// let bgScrollDone = false;
+// window.addEventListener("scroll", () => {
+//   if (!airplaneAnimationDone && !bgScrollDone && window.scrollY > 200) {
+//     bgContainer.style.position = "fixed";
+//     bgContainer.style.top = "0";
+//     bgScrollDone = true;
+//   }
+//   if (airplaneAnimationDone && bgScrollDone) {
+//     bgContainer.style.position = "static";
+//   }
+// });
